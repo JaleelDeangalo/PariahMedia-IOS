2a9d4c7d27f37d3a91772e202aa089937427380a
Jaleel DeAngalo Gilbert <jaleeldeangalo@gmail.com>
2022-11-11 07:47:03 -0500

    Initial Commit

---
+++ PariahMedia/PariahMediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PariahMediaApp.swift
+//  PariahMedia
+//
+//  Created by Jaleel Gilbert on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct PariahMediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
